bb9a49611b58ff766436e2f6605a4a980a3acf7f
amit-khatke <khatkeamit28@gmail.com>
2025-07-16 10:42:42 +0530

    all api are completed

--- projectmanagementsystem/src/main/java/com/amit/projectmanagementsystem/repository/CommentRepository.java
+++ projectmanagementsystem/src/main/java/com/amit/projectmanagementsystem/repository/CommentRepository.java
@@ -2,10 +2,14 @@ package com.amit.projectmanagementsystem.repository;
 
 import com.amit.projectmanagementsystem.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
-    List<Comment> findCommentByIssue(Long issueId);
+    @Query("SELECT c FROM Comment c WHERE c.issue.id = :issueId")
+    List<Comment> findCommentByIssue(@Param("issueId") Long issueId);
+
 }
